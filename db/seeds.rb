c367fcb22f61103010a77ee64451d10901c47c5e
Christelle Revol <christellerevol.works@gmail.com>
2025-03-05 17:19:49 +0100

    advanced routing exercice 3 commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts 'cleaning database'
+Flat.destroy_all
+
+puts 'creating flats'
+
+5.times do
+  Flat.create!(
+    name: Faker::JapaneseMedia::CowboyBebop.episode,
+    address: Faker::Fantasy::Tolkien.location,
+    description: Faker::Movies::VForVendetta.quote,
+    price_per_night: "#{rand(40..200)}€",
+    number_of_guests: "#{rand(1..9)} people"
+  )
+end
+
+puts 'done'
+puts "#{Flat.count} flats created"
